732d9cc558e3096f5b7b4cc0d16991d49f072232
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2022-09-07 13:58:02 +0200

    Update Les_objets_en_python.py

--- Les_objets_en_python.py
+++ Les_objets_en_python.py
@@ -26,8 +26,35 @@ def multiplie1(liste):
 print(multiplie1([1,1.2,-14])) # [2, 2.4, -28]
 
 
-def multiplie2(tabl):
-    tabl1=[3*item+4 for item in tabl]
-    return(tabl1)
+def multiplie2(tabx):
+    tablx1=[3*item+4 for item in tabx]
+    return(tablx1)
 
 print(multiplie2([1,1.2,-14])) # [7, 7.6, -38]
+
+
+def voyelle_mot(mot):
+    tab_voy = []
+    for letter in mot:
+        if letter in "aeiouyAEIOUY":
+            tab_voy.append(letter)
+    return tab_voy
+
+print(voyelle_mot("Acadabra")) # ['A', 'a', 'a', 'a']
+
+
+def voyelle_mot1(mot):
+    tab_voy = [letter for letter in mot if letter in "aeiouyAEIOUY"]
+    return tab_voy
+
+print(voyelle_mot1("Acadabra"))
+#ayant pour parametre une phrase appelee phrase et qui retourne le nombre de voyelles et qui retourne le nombre nrb_voy = au nombre de voyelles de la phrase
+
+def nb_voyelle_mot(mot):
+    nb_voy = 0
+    for letter in mot:
+        if letter in "aeiouyAEIOUY":
+            nb_voy += 1
+    return nb_voy
+
+print(nb_voyelle_mot("Acadabra"))
