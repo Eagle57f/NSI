92ce211b60112a43a5af3c4bb4db8e3daf8c8911
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2022-09-08 15:36:02 +0200

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -1,1 +1,18 @@
+from turtle import *
 
+speed(10)
+
+
+def square(*, pos:tuple, lenght:int):
+    up()
+    goto(pos)
+    down()
+    for i in range(4):
+        fd(lenght)
+        lt(90)
+
+square(pos=(0, 0), lenght=50)
+square(pos=(-200, 200), lenght=50)
+square(pos=(-200, -200), lenght=50)
+square(pos=(200, -200), lenght=50)
+square(pos=(200, 200), lenght=50)
