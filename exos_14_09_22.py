9cd993a98aea819cd54eae50c31029a054c4f150
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2022-09-15 16:45:13 +0200

    Update exos_14_09_22.py

--- exos_14_09_22.py
+++ exos_14_09_22.py
@@ -1,86 +1,102 @@
-###"Exercice 1######
+def lettres_mot(mot):
+    return [l for l in mot if l not in "-'"]
+
+# print(lettres_mot("1019192029"))
 
 
-####Effectuer en système hexadécimal les sommes suivantes:
-####  afe125+de15f et vérifiez votre résultat sur python
-####  bdcfe15+abc36f
+def dec_bin(nb):
+    t = int(str(nb), 2)
+    return t
 
-# print(hex(0xafe125+0xde15f)) # bdc284
-
-# print(hex(0xbdcfe15+0xabc36f)) # c88c184
+# print(dec_bin(10000))
 
 
-
-###Exercice 2####
-### Ecrire une fonction dec_bin ayant pour paramètre nombre et qui
-### retourne la chaine de caractères binaire qui est l'écriture binaire du nombre
-
-def dec_bin(nombre:int):
-    bin_nb = ""
-    while nombre//2 != 0:
-        bin_nb = str(nombre%2) + bin_nb
-        nombre //= 2
-    bin_nb = str(nombre%2) + bin_nb
-    return bin_nb
-
-# print(dec_bin(1234)) # 10011010010
-# print(bin(1234)) # 10011010010
-
-### dec_bin(16) donne 10000
-### pour tester bin (16)=0b10000
-
-# print(dec_bin(16)) # 10000
-
-###Exercice 3####
-###même exercice que 2 mais la fonction dec_bin_tab retourne un tableau contenant
-###dans l'ordre les chiffres de l'écriture de nombre en binaire
-##"on pourra utiliser l'exercice2
-
-def dec_bin1(nombre:int):
-    bin_nb = []
-    while nombre//2 != 0:
-        bin_nb.insert(0, str(nombre%2))
-        nombre //= 2
-    bin_nb.insert(0, str(nombre%2))
-    return bin_nb
-
-# print(dec_bin1(1234)) # ['1', '0', '0', '1', '1', '0', '1', '0', '0', '1', '0']
-# print(bin(1234)) # 10011010010
+def dec_bin1(nb):
+    b = ""
+    while nb // 2 != 0:
+        b = str(nb%2) + b
+        nb //= 2
+    b = str(nb%2) + b
+    return b
 
 
+# print(dec_bin1(16))
 
-###Exercice 4 ####
-###Ecrire une fonction somme_hexa ayant pour paramètre deux nombres n et m
-### m et n sont des nombres en hexadécimal
-### somme_hexa doit retourner la somme de n et m en héxadécimal.
+def tab_chiffre(nb):
+    return lettres_mot(dec_bin1(nb))
 
-def somme_hexa(n, m):
-    if type(n) is str:
-        n = int(n,16)
-    if type(m) is str:
-        m = int(m,16)
-    number = n + m
+# print(tab_chiffre(16))
 
-    hex_num = ""
-    def tohex(number, hex_num, b):
-        nb=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-        if number<=1:
-            hex_num = str(number) + hex_num
-            b = True
+def tab_chiffre1(nb):
+    return lettres_mot(bin(nb))[2:]
+
+# print(tab_chiffre1(16))
+
+import turtle as tl
+tl.speed(100)
+
+def decime_binaire(nb: int): # fonction principale
+
+    def dec_bin2(nb): # fonction qui retourne le binaire (sans 0b) d'un décimal
+        b = ""
+        while nb // 2 != 0:
+            b = str(nb%2) + b
+            nb //= 2
+        b = str(nb%2) + b
+        return b
+
+
+    def ca(color): # fonction qui dessine le carré de couleur donnée
+        tl.begin_fill()
+        tl.fillcolor(color)
+        for c in range(4):
+            tl.fd(20)
+            tl.lt(90)
+        tl.fd(20)
+        tl.end_fill()
+
+    tl.up()
+    tl.goto(-250,0)
+    tl.down()
+
+    for i in dec_bin2(nb): # boucle qui parcours le binaire
+        if i == "0":
+            ca("black")
         else:
-            x = number%16
-            number = number//16
-            if x < 10:
-                hex_num = str(x) + hex_num
-            if x >= 10:
-                hex_num = str(nb[x]) + hex_num
-        return number, hex_num, b
-    b = False
-    while number//16 != 0 and b == False:
-        number, hex_num, b = tohex(number, hex_num, b)
-    number, hex_num, b = tohex(number, hex_num, b)
+            ca("white")
 
-    return hex_num
+# decime_binaire(123)
+# print(bin(123))
 
 
-print(somme_hexa("0x1f04d", 0x561f1a)) # Prend en paramètres des hexadécimaux, strings avec 0x ou sans.
+
+def decime_hexa(nb: int): # fonction principale
+
+    def ca(color, l): # fonction qui dessine le carré de couleur donnée
+        tl.begin_fill()
+        tl.fillcolor(color[0])
+        for c in range(3):
+            tl.fd(50)
+            tl.lt(90)
+        tl.fd(40)
+        tl.pencolor(color[1])
+        tl.write("   " + l,font=("Verdana",15, "normal"))
+        tl.pencolor("black")
+        tl.fd(10)
+        tl.lt(90)
+        tl.fd(50)
+        tl.end_fill()
+
+    tl.up()
+    tl.goto(-250,0)
+    tl.down()
+
+    for i in hex(nb).replace("0x", ""): # boucle qui parcours le binaire
+        d = {"0":("black", "white"), "1":("white", "black"), "2":("blue", "white"), "3":("yellow", "black"), "4":("red", "white"), "5":("green", "white"), "6":("pink", "black"), "7":("purple", "white"), "8":("dark blue", "white"), "9":("grey", "black"), "a":("brown", "white"), "b":("cyan", "black"), "c":("yellow", "black"), "d":("red", "white"), "e":("green", "white"), "f":("pink", "black")}
+        ca(d[i], i)
+
+decime_hexa(81985529216486895)
+print(hex(81985529216486895))
+
+tl.exitonclick()
+print(0x0123456789abcdef)
