8a0dd11a2fa978b5b93730fb1d76a0c1de8f8076
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2022-09-15 21:24:19 +0200

    Update exos_14_09_22.py

--- exos_14_09_22.py
+++ exos_14_09_22.py
@@ -35,38 +35,5 @@ def tab_chiffre1(nb):
 import turtle as tl
 tl.speed(100)
 
-def decime_binaire(nb: int): # fonction principale
-
-    def dec_bin2(nb): # fonction qui retourne le binaire (sans 0b) d'un décimal
-        b = ""
-        while nb // 2 != 0:
-            b = str(nb%2) + b
-            nb //= 2
-        b = str(nb%2) + b
-        return b
-
-
-    def ca(color): # fonction qui dessine le carré de couleur donnée
-        tl.begin_fill()
-        tl.fillcolor(color)
-        for c in range(4):
-            tl.fd(20)
-            tl.lt(90)
-        tl.fd(20)
-        tl.end_fill()
-
-    tl.up()
-    tl.goto(-250,0)
-    tl.down()
-
-    for i in dec_bin2(nb): # boucle qui parcours le binaire
-        if i == "0":
-            ca("black")
-        else:
-            ca("white")
-
-# decime_binaire(123)
-# print(bin(123))
-
 
 
