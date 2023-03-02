8a9af46c0180478c630ed64b92ce8dcb2d00168a
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2023-03-02 15:38:25 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,97 @@
+def recherche(c, mot):
+    i=0
+    for car in mot:
+        if car == c:
+            i+=1
+    print(i)
+    return i
+
+# recherche("a", "mississipi")
+
+
+def recherche2(c, mot):
+    return len([car for car in mot if car == c])
+
+# print(recherche2("a", "Alibaba"))
+
+
+
+def moyenne(list_notes):
+    somme_notes = 0
+    somme_coeffs = 0
+    for note in list_notes:
+        somme_notes += (note[0] * note[1])
+        somme_coeffs += note[1]
+    
+    return "Absent" if not somme_notes == 0 else somme_notes/somme_coeffs
+
+# print(moyenne([(15,2), (9,1), (12,3)]))
+# print(moyenne([]))
+
+
+def moyenne2(list_notes):
+    somme_notes = sum([note[0] * note[1] for note in list_notes])
+    somme_coeffs = sum([note[1] for note in list_notes])
+    
+    return "Absent" if somme_notes == 0 else somme_notes/somme_coeffs
+
+# print(moyenne2([(15,2), (9,1), (12,3)]))
+# print(moyenne2([]))
+
+
+
+def recherche3(list_t):
+    list_res = []
+    for i in range(len(list_t)-1):
+        if list_t[i+1]-1 == list_t[i]:
+            list_res.append((list_t[i], list_t[i+1]))
+    return list_res
+
+# print(recherche3([1,2,3,4,2,5,9,3,1]))
+
+
+def recherche4(list_t):
+    return [(list_t[i], list_t[i+1]) for i in range(len(list_t)-1) if list_t[i+1]-1 == list_t[i]]
+
+# print(recherche4([1,2,3,2,4,2,5,9,3,1]))
+
+
+
+
+def tadaro(l):
+    d = {}
+    for letter in l:
+        if letter in d:
+            d[letter] += 1
+        else:
+            d[letter] = 1
+    l_end = []
+    for letter in d:
+        l_end.append((letter, d[letter]))
+    return l_end
+
+
+# print(tadaro(["a", "c", "c", "c", "o", "e", "a", "i"]))
+
+
+
+def tadaro(l):
+    l_end = []
+    for letter in l:
+        is_in = False
+        for letter_count in l_end:
+            if letter_count[0] == letter:
+                is_in = True
+                letter_count[1] += 1
+                break
+        if is_in == False:
+            l_end.append([letter, 1])
+    l_tpl_end = []
+    for tpl in l_end:
+        l_tpl_end.append(tuple(tpl))
+    return l_tpl_end
+            
+
+
+print(tadaro(["a", "c", "c", "c", "o", "e", "a", "i"]))
+
